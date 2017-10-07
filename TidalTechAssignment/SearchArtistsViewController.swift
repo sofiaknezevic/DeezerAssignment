16711c14c96dff47887f70c99ecab1d5fa1cf46a
sofiaknezevic <sofia.knezevic@gmail.com>
2017-10-07 12:26:42 -0700

    Add SearchArtists View Controller class, remove old view controller

--- TidalTechAssignment/SearchArtistsViewController.swift
+++ TidalTechAssignment/SearchArtistsViewController.swift
@@ -1,25 +1,25 @@
 //
-//  ViewController.swift
+//  SearchArtistsViewController.swift
 //  TidalTechAssignment
 //
-//  Created by Sofia Knezevic on 2017-10-04.
+//  Created by Sofia Knezevic on 2017-10-07.
 //  Copyright © 2017 Sofia Knezevic. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class SearchArtistsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
 
 }
-
