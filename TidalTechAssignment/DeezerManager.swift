c8954faf8487dc8bd0d103faf91bf3be6997fe29
sofiaknezevic <sofia.knezevic@gmail.com>
2017-10-07 12:13:38 -0700

    Add deezermanager class

---
+++ TidalTechAssignment/DeezerManager.swift
@@ -0,0 +1,13 @@
+//
+//  DeezerManager.swift
+//  TidalTechAssignment
+//
+//  Created by Sofia Knezevic on 2017-10-07.
+//  Copyright © 2017 Sofia Knezevic. All rights reserved.
+//
+
+import UIKit
+
+class DeezerManager: NSObject {
+
+}
