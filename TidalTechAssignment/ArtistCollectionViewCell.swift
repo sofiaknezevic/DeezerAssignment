46f0a024263a9e2d33fa9031c214951c3e038930
sofiaknezevic <sofia.knezevic@gmail.com>
2017-10-09 19:16:12 -0700

    Add AFNetworking pod

--- TidalTechAssignment/ArtistCollectionViewCell.swift
+++ TidalTechAssignment/ArtistCollectionViewCell.swift
@@ -13,13 +13,13 @@ class ArtistCollectionViewCell: UICollectionViewCell {
     //MARK: - Initializers
     override init(frame: CGRect) {
         super.init(frame: frame)
-        contentView.addSubview(artistNameLabel)
+        contentView.addSubview(containerStackView)
         artistNameLabel.translatesAutoresizingMaskIntoConstraints = false
         
-        artistNameLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 8).isActive = true
-        artistNameLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -8).isActive = true
-        artistNameLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 8).isActive = true
-        artistNameLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -8).isActive = true
+        containerStackView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 8).isActive = true
+        containerStackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -8).isActive = true
+        containerStackView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 8).isActive = true
+        containerStackView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -8).isActive = true
     }
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
@@ -28,13 +28,29 @@ class ArtistCollectionViewCell: UICollectionViewCell {
     //MARK: - Configuration
     func configureCell(artist:DeezerArtist) {
         artistNameLabel.text = artist.artistName
+        artistImageView.image = UIImage
     }
     
-    //MARK: - Lazy Initializer Variables
+    //MARK: - Lazy Initializer Variables -
+    //MARK: - Label
     private lazy var artistNameLabel:UILabel = {
         let artistNameLabel = UILabel()
         artistNameLabel.textColor = .white
         artistNameLabel.font = UIFont.systemFont(ofSize: 10)
         return artistNameLabel
+    }()
+    //MARK: - ImageView
+    private lazy var artistImageView:UIImageView = {
+        let artistImageView = UIImageView()
+        return artistImageView
+    }()
+    //MARK: - Container StackView
+    private lazy var containerStackView:UIStackView = {
+        let containerStackView = UIStackView(arrangedSubviews: [self.artistImageView, self.artistNameLabel])
+        containerStackView.axis = .horizontal
+        containerStackView.alignment = .fill
+        containerStackView.distribution = .fillProportionally
+        containerStackView.spacing = 5
+        return containerStackView
     }()
 }
