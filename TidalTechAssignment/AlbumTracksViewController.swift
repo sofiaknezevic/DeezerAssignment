55c95c3aa23bbf6fe817923f43054d9946a583f4
sofiaknezevic <sofia.knezevic@gmail.com>
2017-10-10 22:06:05 -0700

    Add albumtracks viewcontroller

---
+++ TidalTechAssignment/AlbumTracksViewController.swift
@@ -0,0 +1,35 @@
+//
+//  AlbumTracksViewController.swift
+//  TidalTechAssignment
+//
+//  Created by Sofia Knezevic on 2017-10-10.
+//  Copyright © 2017 Sofia Knezevic. All rights reserved.
+//
+
+import UIKit
+
+class AlbumTracksViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
