6fc0a65315c4b2e52c9dda6774fdce52fcfcdb91
sofiaknezevic <sofia.knezevic@gmail.com>
2017-10-10 22:08:19 -0700

    Add DeezerTrack object class to handle all track objects

---
+++ TidalTechAssignment/DeezerTrack.swift
@@ -0,0 +1,13 @@
+//
+//  DeezerTrack.swift
+//  TidalTechAssignment
+//
+//  Created by Sofia Knezevic on 2017-10-10.
+//  Copyright © 2017 Sofia Knezevic. All rights reserved.
+//
+
+import UIKit
+
+class DeezerTrack: NSObject {
+
+}
