90bde44a113dcc38980b371205768022e58c1e9d
sofiaknezevic <sofia.knezevic@gmail.com>
2017-10-10 21:02:31 -0700

    Add necessary UI variables to the topbarview

--- TidalTechAssignment/TopBarView.swift
+++ TidalTechAssignment/TopBarView.swift
@@ -9,7 +9,40 @@
 import UIKit
 
 class TopBarView: UIView {
-
-
+    
+    let moreMenuButton = UIButton()
+    let searchIconImageView = UIImageView()
+    let searchIconContainerView = UIView()
+    
+    let topLabel = UILabel()
+    let bottomLabel = UILabel()
+    
+    //MARK: - Initializers
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addSubview(containerStackView)
+    }
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    //MARK: - Lazy Initializer Variables
+    //MARK: - Stack View
+    private lazy var containerStackView:UIStackView = {
+        let containerStackView = UIStackView()
+        containerStackView.axis = .horizontal
+        containerStackView.alignment = .fill
+        containerStackView.distribution = .fillProportionally
+        containerStackView.spacing = 5
+        return containerStackView
+    }()
+    private lazy var labelStackView:UIStackView = {
+        let labelStackView = UIStackView(arrangedSubviews: [self.topLabel, self.bottomLabel])
+        labelStackView.axis = .vertical
+        labelStackView.alignment = .fill
+        labelStackView.distribution = .fillProportionally
+        labelStackView.spacing = 5
+        return labelStackView
+    }()
 
 }
